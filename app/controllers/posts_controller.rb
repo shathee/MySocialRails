fe6d4846c5bc80a22a5670c2ead3d1fcc51a7030
Baki <sat.sust@gmail.com>
2021-05-09 00:12:26 +0200

    sign in sign up done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!
 
+  
+
 
   # GET /posts or /posts.json
   def index
@@ -16,7 +18,8 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_user.post.build
+    # @post = Post.new
   end
 
   # GET /posts/1/edit
@@ -25,7 +28,8 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    # @post = Post.new(post_params)
+    @post = current_user.post.build(post_params)
 
     respond_to do |format|
       if @post.save
